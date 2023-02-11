ac39c8edddfcb6d3c0c0d5db56e8beee0b922f5f
Farna Chilashvili <farnnn.ff@gmail.com>
2023-02-11 14:46:16 +0400

    Task 1

--- task-1/index.js
+++ task-1/index.js
@@ -1,11 +1,9 @@
-function solve(st,a,b) {
+function solve(st, a, b) {
     let part1 = st.slice(0, a)
     let part2 = st.substring(a, b + 1);
-    let exploded = part2.split('');
-    let reversed = exploded.reverse();
-    let reversedString = reversed.join('')
-    let part3 = st.substring(b)
+    let reversedString = part2.split('').reverse().join('')
+    let part3 = st.substring(b + 1)
     return part1 + reversedString + part3
 }
 
-solve("developer",1,5)
+solve("developer", 1, 5)
